da0a926639aefa1be3657b05fb94d6fd1722c6f3
Maksym <maximkack@gmail.com>
2023-02-04 17:14:15 -0500

    game

---
+++ modules/api/clients/github.py
@@ -0,0 +1,10 @@
+import requests
+
+
+class GitHub:
+
+    def get_user_defunkt(self):
+        r = requests.get('https://api.github.com/users/defunkt')
+        body = r.json()
+
+        return body
